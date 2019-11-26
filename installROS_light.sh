22549748172d686d9dd5bdc4d48f55a32a457bd9
Elia Bruni <elia.bruni@gmail.com>
2019-11-27 00:31:28 +0100

    Update installROS_light.sh
    Moved from apt to apt-get

--- installROS_light.sh
+++ installROS_light.sh
@@ -6,9 +6,9 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-sudo apt update 
+sudo apt-get update 
 
-sudo apt install ros-melodic-ros-base -y -q --no-install-recommends
+sudo apt-get install ros-melodic-ros-base -y -q --no-install-recommends
 
 #echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 #source ~/.bashrc
@@ -18,5 +18,5 @@ sudo apt install ros-melodic-ros-base -y -q --no-install-recommends
 #echo "source /opt/ros/melodic/setup.zsh" >> ~/.zshrc
 #source ~/.zshrc
 
-#sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y -q --no-install-recommend
+#sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y -q --no-install-recommend
 
